c0c485eaf89cf0e881c513fe2c235aa432e5c25a
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-12-20 08:23:03 +0530

    Add Python Project

---
+++ PythonProject/myapp.py
@@ -0,0 +1,6 @@
+print('Program to sum two numbers: ')
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+result = num1 + num2
+
+print(f'Sum of two numbers are: {result}')
