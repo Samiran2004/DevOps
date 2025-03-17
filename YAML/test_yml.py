81ab1c40fb65d2f9fad214d2f3c6cc2c59125e2c
Samiran2004 <samirans170@gmail.com>
2025-03-17 22:48:46 +0530

    add/demo docker-compose file

--- YAML/test_yml.py
+++ YAML/test_yml.py
@@ -1,6 +1,9 @@
 import yaml
 
-with open('first.yml', 'r') as file:
+#file_name = 'first.yml'
+file_name = 'demo_docker-compose.yml'
+
+with open(file_name, 'r') as file:
     try:
         print(yaml.safe_load(file))
     except yaml.YAMLError as error:
